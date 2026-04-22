607b27d72964d1256ca04ce7903b9ce6b5e998d1
abday_hafidz <1123150093@global.ac.id>
2026-04-22 16:59:19 +0700

    style: membuat reusable widget google sign in

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,15 +1,13 @@
-class AppConstants{
-  // http://192.168.100.5:8080/v1
+class AppConstants {
   static const String baseUrl = 'http://localhost:8080/v1';
- 
+
   // Auth endpoints
   static const String verifyToken = '/auth/verify-token';
- 
+
   // Product endpoints
   static const String products = '/products';
- 
+
   // Timeout
   static const int connectTimeout = 15000;
   static const int receiveTimeout = 15000;
-
 }
